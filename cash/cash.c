5d6cf6d44d22648e5438416ef169c85aa7cc8e28
Vest53 <matheus.moreschicostada@gmail.com>
2024-11-13 18:11:00 +0000

    Wed, Nov 13, 2024, 3:11 PM -03:00

--- cash/cash.c
+++ cash/cash.c
@@ -15,14 +15,17 @@ int main() {
         if (change < 1) {
             printf("Please enter a value greater than 0.\n");
         }
-    } while (change < 1);
+    } while (change < 1);// Repeat until input is valid
 
 
+// Variables for counting coins
     int coins = 0;
 
+// Available currencies
     int coinsAvailable[] = {25, 10, 5, 1};
     int numCoins = sizeof(coinsAvailable) / sizeof(coinsAvailable[0]);
 
+// Calculate the minimum number of coins
     for (int i = 0; i < numCoins; i++) {
         while (change >= coinsAvailable[i]) {
             change -= coinsAvailable[i];
@@ -31,6 +34,7 @@ int main() {
     }
 
 
+// Print the minimum number of coins
     printf("%d\n", coins);
 
     return 0;
