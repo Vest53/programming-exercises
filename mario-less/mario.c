0734bd3fba9df036a4e529fb48cba287a5f33b39
Vest53 <matheus.moreschicostada@gmail.com>
2024-11-13 18:07:22 +0000

    Wed, Nov 13, 2024, 3:07 PM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,18 +1,38 @@
+
 #include <stdio.h>
 
 int main() {
-    int height, i, j;
+    int height;
+    char buffer[10]; // To store user input
 
-    printf("Enter the height of the pyramid: ");
-    scanf("%d", &height);
+// Request the height of the pyramid from the user
+    while (1) {
+        printf("Digite a altura da pirâmide: ");
+        fgets(buffer, sizeof(buffer), stdin); // Read input as string
 
-    for (i = 1; i <= height; i++) {
-        for (j = i; j < height; j++) {
+        //Attempts to convert the input to an integer
+        if (sscanf(buffer, "%d", &height) != 1 || height < 1) {
+            printf("Please enter a positive integer greater than 0.\n");
+        } else {
+            break; //Exit the loop if the input is valid
+
+        }
+    }
+
+    // Loop for each row of the pyramid
+
+    for (int i = 1; i <= height; i++) {
+        // Print the blanks
+
+        for (int j = i; j < height; j++) {
             printf(" ");
         }
-        for (j = 1; j <= i; j++) {
+        // Print the hashes
+
+        for (int j = 1; j <= i; j++) {
             printf("#");
         }
+        // New line after each line of the pyramid
         printf("\n");
     }
 
