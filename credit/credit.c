671b7c3b3229baaa068f9ae8bd1f6fa63a886ca0
Vest53 <matheus.moreschicostada@gmail.com>
2024-11-19 14:21:55 +0000

    Tue, Nov 19, 2024, 11:21 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,17 +1,20 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 long get_card_number();
 int is_valid(long number);
 int get_card_type(long number);
 
-int main(void) {
+int main(void)
+{
     long card_number = get_card_number();
 
-    if (is_valid(card_number)) {
+    if (is_valid(card_number))
+    {
         int card_type = get_card_type(card_number);
-        switch (card_type) {
+        switch (card_type)
+        {
             case 1:
                 printf("AMEX\n");
                 break;
@@ -25,31 +28,40 @@ int main(void) {
                 printf("INVALID\n");
                 break;
         }
-    } else {
+    }
+    else
+    {
         printf("INVALID\n");
     }
 }
 
-long get_card_number() {
+long get_card_number()
+{
     long number;
-    do {
+    do
+    {
         number = get_long("Number: ");
-    } while (number <= 0);
+    }
+    while (number <= 0);
     return number;
 }
 
-int is_valid(long number) {
+int is_valid(long number)
+{
     int sum = 0;
     int count = 0;
     long temp = number;
 
     // Luhn's algorithm
-    while (temp > 0) {
+    while (temp > 0)
+    {
         count++;
         int digit = temp % 10;
-        if (count % 2 == 0) {
+        if (count % 2 == 0)
+        {
             digit *= 2;
-            if (digit > 9) {
+            if (digit > 9)
+            {
                 digit -= 9;
             }
         }
@@ -60,12 +72,14 @@ int is_valid(long number) {
     return (sum % 10 == 0);
 }
 
-int get_card_type(long number) {
+int get_card_type(long number)
+{
     // Get the first two digits and the total number of digits
     int num_digits = 0;
     long temp = number;
 
-    while (temp > 0) {
+    while (temp > 0)
+    {
         num_digits++;
         temp /= 10;
     }
@@ -74,13 +88,18 @@ int get_card_type(long number) {
     int first_two_digits = number / (long) pow(10, num_digits - 2);
 
     // Determine the card type
-    if (num_digits == 15 && (first_two_digits == 34 || first_two_digits == 37)) {
-        return 1;  // AMEX
-    } else if (num_digits == 16 && (first_two_digits >= 51 && first_two_digits <= 55)) {
-        return 2;  // MASTERCARD
-    } else if ((num_digits == 13 || num_digits == 16) && first_digit == 4) {
-        return 3;  // VISA
+    if (num_digits == 15 && (first_two_digits == 34 || first_two_digits == 37))
+    {
+        return 1; // AMEX
+    }
+    else if (num_digits == 16 && (first_two_digits >= 51 && first_two_digits <= 55))
+    {
+        return 2; // MASTERCARD
+    }
+    else if ((num_digits == 13 || num_digits == 16) && first_digit == 4)
+    {
+        return 3; // VISA
     }
 
-    return 0;  // INVALID
+    return 0; // INVALID
 }
