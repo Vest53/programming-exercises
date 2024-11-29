4b64b602f5ebe4dda0ec1cd8a8e6332d945d7ed1
Vest53 <matheus.moreschicostada@gmail.com>
2024-11-29 17:02:43 +0000

    Fri, Nov 29, 2024, 2:02 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,23 +1,28 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-void encrypt(char *plaintext, int key) {
+void encrypt(char *plaintext, int key)
+{
     char ciphertext[strlen(plaintext) + 1]; // Create a buffer for ciphertext
-    for (int i = 0; plaintext[i] != '\0'; i++) {
+    for (int i = 0; plaintext[i] != '\0'; i++)
+    {
         char current_char = plaintext[i];
 
         // Encrypt uppercase letters
-        if (isupper(current_char)) {
+        if (isupper(current_char))
+        {
             ciphertext[i] = (current_char - 'A' + key) % 26 + 'A';
         }
         // Encrypt lowercase letters
-        else if (islower(current_char)) {
+        else if (islower(current_char))
+        {
             ciphertext[i] = (current_char - 'a' + key) % 26 + 'a';
         }
         // Leave non-alphabetic characters unchanged
-        else {
+        else
+        {
             ciphertext[i] = current_char;
         }
     }
@@ -25,16 +30,19 @@ void encrypt(char *plaintext, int key) {
     printf("Ciphertext: %s\n", ciphertext);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Check if the user provided a key
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
     // Convert key to an integer
     int key = atoi(argv[1]);
-    if (key < 0) {
+    if (key < 0)
+    {
         printf("Key must be a non-negative integer\n");
         return 1;
     }
