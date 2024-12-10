f6a44c4482e264138acd2dc3185da6e95f54c3c1
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-10 03:06:15 +0000

    Tue, Dec 10, 2024, 12:06 AM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -4,15 +4,22 @@
 #define MAX_CANDIDATES 9
 
 // Estruturas
-typedef struct {
+typedef struct
+{
+    char name[50];
+} Candidate;
+
+typedef struct
+{
     int winner;
     int loser;
 } Pair;
 
 // Variáveis globais
+Candidate candidates[MAX_CANDIDATES];
+Pair pairs[MAX_CANDIDATES * (MAX_CANDIDATES - 1) / 2];
 int preferences[MAX_CANDIDATES][MAX_CANDIDATES] = {{0}};
 int locked[MAX_CANDIDATES][MAX_CANDIDATES] = {{0}};
-Candidate candidates[MAX_CANDIDATES];
 int pair_count = 0;
 
 // Prototipagem de funções
@@ -24,14 +31,16 @@ void lock_pairs();
 int is_cycle(int winner, int loser);
 void print_winner(int candidate_count);
 
-int main() {
+int main()
+{
     // Leitura do número de candidatos
     int candidate_count;
     printf("Informe o número de candidatos: ");
     scanf("%d", &candidate_count);
 
     // Leitura dos nomes dos candidatos
-    for (int i = 0; i < candidate_count; i++) {
+    for (int i = 0; i < candidate_count; i++)
+    {
         printf("Nome do candidato %d: ", i + 1);
         scanf("%s", candidates[i].name);
     }
@@ -40,7 +49,8 @@ int main() {
     int voter_count;
     printf("Informe o número de eleitores: ");
     scanf("%d", &voter_count);
-    for (int i = 0; i < voter_count; i++) {
+    for (int i = 0; i < voter_count; i++)
+    {
         int ranks[MAX_CANDIDATES];
         vote(ranks, candidate_count);
         record_preferences(ranks, candidate_count);
@@ -57,29 +67,39 @@ int main() {
 }
 
 // Função para registrar os votos
-void vote(int ranks[], int candidate_count) {
+void vote(int ranks[], int candidate_count)
+{
     // Exemplo de implementação para coletar votos
-    for (int i = 0; i < candidate_count; i++) {
+    for (int i = 0; i < candidate_count; i++)
+    {
         printf("Classifique o candidato %d (0 a %d): ", i + 1, candidate_count - 1);
         scanf("%d", &ranks[i]);
     }
 }
 
 // Função para registrar as preferências
-void record_preferences(int ranks[], int candidate_count) {
-    for (int i = 0; i < candidate_count; i++) {
-        for (int j = i + 1; j < candidate_count; j++) {
+void record_preferences(int ranks[], int candidate_count)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
             preferences[ranks[i]][ranks[j]]++;
         }
     }
 }
 
 // Função para adicionar pares
-void add_pairs(int candidate_count) {
-    for (int i = 0; i < candidate_count; i++) {
-        for (int j = 0; j < candidate_count; j++) {
-            if (i != j) {
-                if (preferences[i][j] > preferences[j][i]) {
+void add_pairs(int candidate_count)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (i != j)
+            {
+                if (preferences[i][j] > preferences[j][i])
+                {
                     pairs[pair_count].winner = i;
                     pairs[pair_count].loser = j;
                     pair_count++;
@@ -90,26 +110,32 @@ void add_pairs(int candidate_count) {
 }
 
 // Função para ordenar pares
-void sort_pairs() {
+void sort_pairs()
+{
     // Implementar ordenação baseada na força dos pares
 }
 
 // Função para bloquear pares
-void lock_pairs() {
-    for (int i = 0; i < pair_count; i++) {
-        if (!is_cycle(pairs[i].winner, pairs[i].loser)) {
+void lock_pairs()
+{
+    for (int i = 0; i < pair_count; i++)
+    {
+        if (!is_cycle(pairs[i].winner, pairs[i].loser))
+        {
             locked[pairs[i].winner][pairs[i].loser] = 1;
         }
     }
 }
 
 // Função para verificar ciclos
-int is_cycle(int winner, int loser) {
+int is_cycle(int winner, int loser)
+{
     // Implementar lógica para detectar ciclos
     return 0; // Placeholder
 }
 
 // Função para imprimir o vencedor
-void print_winner(int candidate_count) {
+void print_winner(int candidate_count)
+{
     // Implementar lógica para encontrar e imprimir o vencedor
 }
