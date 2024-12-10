7ade4da3e10134291b3a0ba85c53e5442682252b
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-10 03:27:12 +0000

    Tue, Dec 10, 2024, 12:27 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,7 +4,8 @@
 
 #pragma pack(push, 1) // Alinhamento de 1 byte
 
-typedef struct {
+typedef struct
+{
     uint16_t bfType;
     uint32_t bfSize;
     uint16_t bfReserved1;
@@ -12,7 +13,8 @@ typedef struct {
     uint32_t bfOffBits;
 } BITMAPFILEHEADER;
 
-typedef struct {
+typedef struct
+{
     uint32_t biSize;
     int32_t biWidth;
     int32_t biHeight;
@@ -28,7 +30,8 @@ typedef struct {
 
 #pragma pack(pop)
 
-void create_bmp(const char *filename, int width, int height, int **matrix) {
+void create_bmp(const char *filename, int width, int height, int **matrix)
+{
     BITMAPFILEHEADER bfh;
     BITMAPINFOHEADER bih;
 
@@ -53,7 +56,8 @@ void create_bmp(const char *filename, int width, int height, int **matrix) {
 
     // Criando o arquivo BMP
     FILE *file = fopen(filename, "wb");
-    if (!file) {
+    if (!file)
+    {
         fprintf(stderr, "Erro ao criar o arquivo BMP.\n");
         exit(2); // Retornando 2 para indicar erro
     }
@@ -61,19 +65,23 @@ void create_bmp(const char *filename, int width, int height, int **matrix) {
 
     // Escrevendo cabeçalhos no arquivo
     if (fwrite(&bfh, sizeof(BITMAPFILEHEADER), 1, file) != 1 ||
-        fwrite(&bih, sizeof(BITMAPINFOHEADER), 1, file) != 1) {
+        fwrite(&bih, sizeof(BITMAPINFOHEADER), 1, file) != 1)
+    {
         fprintf(stderr, "Erro ao escrever cabeçalhos no arquivo BMP.\n");
         fclose(file);
         exit(2); // Retornando 2 para indicar erro
     }
 
     // Criando a imagem
-    for (int y = height - 1; y >= 0; y--) {
-        for (int x = 0; x < width; x++) {
+    for (int y = height - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < width; x++)
+        {
             uint8_t color = matrix[y][x] ? 0 : 255;
             if (fwrite(&color, sizeof(uint8_t), 1, file) != 1 ||
                 fwrite(&color, sizeof(uint8_t), 1, file) != 1 ||
-                fwrite(&color, sizeof(uint8_t), 1, file) != 1) {
+                fwrite(&color, sizeof(uint8_t), 1, file) != 1)
+            {
                 fprintf(stderr, "Erro ao escrever dados de pixel no arquivo BMP.\n");
                 fclose(file);
                 exit(2); // Retornando 2 para indicar erro
@@ -84,22 +92,27 @@ void create_bmp(const char *filename, int width, int height, int **matrix) {
     fclose(file);
 }
 
-int main() {
+int main()
+{
     int width = 5;
     int height = 5;
 
     // Alocando a matriz
     int **matrix = malloc(height * sizeof(int *));
-    if (matrix == NULL) {
+    if (matrix == NULL)
+    {
         fprintf(stderr, "Erro ao alocar memória para a matriz.\n");
         return 2; // Retornando 2 para indicar erro
     }
 
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < height; i++)
+    {
         matrix[i] = malloc(width * sizeof(int));
-        if (matrix[i] == NULL) {
+        if (matrix[i] == NULL)
+        {
             fprintf(stderr, "Erro ao alocar memória para a linha %d.\n", i);
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++)
+            {
                 free(matrix[j]);
             }
             free(matrix);
@@ -109,15 +122,12 @@ int main() {
 
     // Preenchendo a matriz
     int example_matrix[5][5] = {
-        {0, 1, 1, 1, 0},
-        {1, 0, 0, 0, 1},
-        {1, 0, 0, 0, 1},
-        {1, 0, 0, 0, 1},
-        {0, 1, 1, 1, 0}
-    };
+        {0, 1, 1, 1, 0}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 1, 1, 0}};
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             matrix[i][j] = example_matrix[i][j];
         }
     }
@@ -126,7 +136,8 @@ int main() {
     create_bmp("output.bmp", width, height, matrix);
 
     // Liberar a memória
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < height; i++)
+    {
         free(matrix[i]);
     }
     free(matrix);
