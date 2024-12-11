7367f35947c03f74384d6cb6579d2bb834ce7342
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-11 17:37:02 +0000

    Wed, Dec 11, 2024, 2:37 PM -03:00

--- inheritance/heritage_test.c
+++ inheritance/heritage_test.c
@@ -4,9 +4,13 @@
 #include "heritage.h" // Inclua o cabeçalho
 
 // Testes
-int main() {
+void run_tests() {
     person *p = create_family(3);
     printf(check_size(p, 3) == 7 ? "size_true\n" : "size_false\n"); // 7 para 3 gerações
     free_family(p);
+}
+
+int main() {
+    run_tests();
     return 0;
 }
