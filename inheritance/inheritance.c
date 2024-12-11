bf078f1e0e8a324f4a3c90682ea0d376f3f8e58e
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-11 17:43:49 +0000

    Wed, Dec 11, 2024, 2:43 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -1,22 +1,27 @@
 // heritage.c
 
+#include "heritage.h" // Inclua o cabeçalho
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "heritage.h" // Inclua o cabeçalho
 
-person *create_family(int generations) {
+person *create_family(int generations)
+{
     person *new_person = malloc(sizeof(person));
-    if (new_person == NULL) {
+    if (new_person == NULL)
+    {
         return NULL;
     }
 
-    if (generations > 1) {
+    if (generations > 1)
+    {
         new_person->parents[0] = create_family(generations - 1);
         new_person->parents[1] = create_family(generations - 1);
         new_person->alleles[0] = new_person->parents[0]->alleles[rand() % 2];
         new_person->alleles[1] = new_person->parents[1]->alleles[rand() % 2];
-    } else {
+    }
+    else
+    {
         new_person->parents[0] = NULL;
         new_person->parents[1] = NULL;
         new_person->alleles[0] = "ABO"[rand() % 3];
@@ -25,23 +30,27 @@ person *create_family(int generations) {
     return new_person;
 }
 
-void free_family(person *p) {
-    if (p == NULL) return;
+void free_family(person *p)
+{
+    if (p == NULL)
+        return;
     free_family(p->parents[0]);
     free_family(p->parents[1]);
     free(p);
 }
 
-void print_family(person *p, int generation) {
-    if (p == NULL) return;
+void print_family(person *p, int generation)
+{
+    if (p == NULL)
+        return;
     printf("Geração %d: Tipo sanguíneo: %c%c\n", generation, p->alleles[0], p->alleles[1]);
     print_family(p->parents[0], generation + 1);
     print_family(p->parents[1], generation + 1);
 }
 
-int check_size(person *p, int n) {
-    if (p == NULL) return 0;
+int check_size(person *p, int n)
+{
+    if (p == NULL)
+        return 0;
     return 1 + check_size(p->parents[0], n) + check_size(p->parents[1], n);
 }
-
-
