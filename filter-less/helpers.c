95625a777156312ab3aec1d570ab80806563bdf2
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-12 18:04:13 +0000

    Thu, Dec 12, 2024, 3:04 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,147 +1,125 @@
-#include <stdint.h>
+#include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-#pragma pack(push, 1) // Alinhamento de 1 byte
-
-typedef struct
-{
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffBits;
-} BITMAPFILEHEADER;
+#include <math.h>
+#include <string.h>
 
 typedef struct
 {
-    uint32_t biSize;
-    int32_t biWidth;
-    int32_t biHeight;
-    uint16_t biPlanes;
-    uint16_t biBitCount;
-    uint32_t biCompression;
-    uint32_t biSizeImage;
-    int32_t biXPelsPerMeter;
-    int32_t biYPelsPerMeter;
-    uint32_t biClrUsed;
-    uint32_t biClrImportant;
-} BITMAPINFOHEADER;
-
-#pragma pack(pop)
-
-void create_bmp(const char *filename, int width, int height, int **matrix)
-{
-    BITMAPFILEHEADER bfh;
-    BITMAPINFOHEADER bih;
-
-    // Inicializando cabeçalhos
-    bfh.bfType = 0x4D42;
-    bfh.bfReserved1 = 0;
-    bfh.bfReserved2 = 0;
-    bfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-    bfh.bfSize = bfh.bfOffBits + (width * height * 3);
-
-    bih.biSize = sizeof(BITMAPINFOHEADER);
-    bih.biWidth = width;
-    bih.biHeight = height;
-    bih.biPlanes = 1;
-    bih.biBitCount = 24;
-    bih.biCompression = 0;
-    bih.biSizeImage = 0;
-    bih.biXPelsPerMeter = 2835;
-    bih.biYPelsPerMeter = 2835;
-    bih.biClrUsed = 0;
-    bih.biClrImportant = 0;
-
-    // Criando o arquivo BMP
-    FILE *file = fopen(filename, "wb");
-    if (!file)
-    {
-        fprintf(stderr, "Erro ao criar o arquivo BMP.\n");
-        exit(2); // Retornando 2 para indicar erro
-    }
-    printf("Arquivo BMP criado com sucesso!\n");
-
-    // Escrevendo cabeçalhos no arquivo
-    if (fwrite(&bfh, sizeof(BITMAPFILEHEADER), 1, file) != 1 ||
-        fwrite(&bih, sizeof(BITMAPINFOHEADER), 1, file) != 1)
-    {
-        fprintf(stderr, "Erro ao escrever cabeçalhos no arquivo BMP.\n");
-        fclose(file);
-        exit(2); // Retornando 2 para indicar erro
-    }
-
-    // Criando a imagem
-    for (int y = height - 1; y >= 0; y--)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            uint8_t color = matrix[y][x] ? 0 : 255;
-            if (fwrite(&color, sizeof(uint8_t), 1, file) != 1 ||
-                fwrite(&color, sizeof(uint8_t), 1, file) != 1 ||
-                fwrite(&color, sizeof(uint8_t), 1, file) != 1)
-            {
-                fprintf(stderr, "Erro ao escrever dados de pixel no arquivo BMP.\n");
-                fclose(file);
-                exit(2); // Retornando 2 para indicar erro
-            }
-        }
-    }
-
-    fclose(file);
+    uint8_t rgbtBlue;   // Blue value
+    uint8_t rgbtGreen;  // Green value
+    uint8_t rgbtRed;    // Red value
 }
+RGBTRIPLE;
 
-int main()
+// Function prototypes
+void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+void sepia(int height, int width, RGBTRIPLE image[height][width]);
+void reflect(int height, int width, RGBTRIPLE image[height][width]);
+void blur(int height, int width, RGBTRIPLE image[height][width]);
+
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int width = 5;
-    int height = 5;
-
-    // Alocando a matriz
-    int **matrix = malloc(height * sizeof(int *));
-    if (matrix == NULL)
-    {
-        fprintf(stderr, "Erro ao alocar memória para a matriz.\n");
-        return 2; // Retornando 2 para indicar erro
-    }
-
-    for (int i = 0; i < height; i++)
-    {
-        matrix[i] = malloc(width * sizeof(int));
-        if (matrix[i] == NULL)
-        {
-            fprintf(stderr, "Erro ao alocar memória para a linha %d.\n", i);
-            for (int j = 0; j < i; j++)
-            {
-                free(matrix[j]);
-            }
-            free(matrix);
-            return 2; // Retornando 2 para indicar erro
-        }
-    }
-
-    // Preenchendo a matriz
-    int example_matrix[5][5] = {
-        {0, 1, 1, 1, 0}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 1, 1, 0}};
-
+    // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            matrix[i][j] = example_matrix[i][j];
+            // Calculate the average of red, green, and blue
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+
+            // Update pixel values
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+}
+
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Get original RGB values
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            // Calculate sepia values
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            // Cap values at 255
+            image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
+        }
+    }
+}
+
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all rows
+    for (int i = 0; i < height; i++)
+    {
+        // Loop only until the middle of the row
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap pixels
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a copy of the image
+    RGBTRIPLE copy[height][width];
+
+    // Copy original image to copy
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
         }
     }
 
-    // Criar o arquivo BMP
-    create_bmp("output.bmp", width, height, matrix);
-
-    // Liberar a memória
+    // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
-        free(matrix[i]);
-    }
-    free(matrix);
+        for (int j = 0; j < width; j++)
+        {
+            int redSum = 0, greenSum = 0, blueSum = 0, count = 0;
 
-    printf("Arquivo BMP criado com sucesso!\n");
-    return 0;
+            // Loop over the surrounding pixels
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int new_i = i + di;
+                    int new_j = j + dj;
+
+                    // Check for valid pixel
+                    if (new_i >= 0 && new_i < height && new_j >= 0 && new_j < width)
+                    {
+                        redSum += copy[new_i][new_j].rgbtRed;
+                        greenSum += copy[new_i][new_j].rgbtGreen;
+                        blueSum += copy[new_i][new_j].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            // Calculate average
+            image[i][j].rgbtRed = round(redSum / (float)count);
+            image[i][j].rgbtGreen = round(greenSum / (float)count);
+            image[i][j].rgbtBlue = round(blueSum / (float)count);
+        }
+    }
 }
