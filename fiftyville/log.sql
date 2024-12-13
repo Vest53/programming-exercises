c093704d309558c45f87da0f554712a751fd3783
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-13 18:30:20 +0000

    Fri, Dec 13, 2024, 3:30 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute BETWEEN 15 AND 25;
