e3dcda6d23660bda4d955b63f09498d195bb4929
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-13 17:17:01 +0000

    Fri, Dec 13, 2024, 2:17 PM -03:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,5 +1,12 @@
-SELECT p.name
-FROM people p
-JOIN directors d ON p.id = d.person_id
-JOIN movies m ON d.movie_id = m.id
-WHERE m.title = 'Toy Story';
+SELECT name
+FROM people
+WHERE id IN
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id = (
+        SELECT id
+        FROM movies
+        WHERE title = 'Toy Story'
+    )
+);
--- movies/9.sql
+++ movies/9.sql
@@ -1,6 +1,13 @@
-SELECT DISTINCT p.name
-FROM people p
-JOIN directors d ON p.id = d.person_id
-JOIN movies m ON d.movie_id = m.id
-WHERE m.year = 2004
-ORDER BY p.birth;
+SELECT DISTINCT name
+FROM people
+WHERE id IN
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN (
+        SELECT id
+        FROM movies
+        WHERE year = 2004
+    )
+)
+ORDER BY birth;
