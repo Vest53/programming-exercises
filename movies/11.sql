30b49947ad7ae8a731e95539a9f181c6e59caee3
Vest53 <matheus.moreschicostada@gmail.com>
2024-12-13 17:25:56 +0000

    Fri, Dec 13, 2024, 2:25 PM -03:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,12 +3,13 @@ FROM movies m
 JOIN ratings r ON m.id = r.movie_id
 WHERE m.id IN (
     SELECT movie_id
-    FROM directors d
-    WHERE d.person_id = (
+    FROM stars
+    WHERE person_id = (
         SELECT id
-        FROM people p
-        WHERE p.name = 'Chadwick Boseman'
+        FROM people
+        WHERE name = 'Chadwick Boseman'
     )
 )
 ORDER BY r.rating DESC
 LIMIT 5;  -- Ajuste o limite conforme necessário
+
